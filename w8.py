c846f8aa13c9bb6cc0dfa744700d13cf0a874988
Worachai-10 <139629440+Worachai-10@users.noreply.github.com>
2024-01-22 07:04:37 -0800

    Add files via upload

---
+++ w8.py
@@ -0,0 +1,37 @@
+# ข้อ1
+n = 1
+while n <= 21:
+    print(n, end=', ')
+    n += 2
+
+print()  # เพื่อขึ้นบรรทัดใหม่
+
+# ข้อ2.
+n = 2
+while n <= 17:
+    print(n, end=', ')
+    n += 3
+
+print()
+
+# ข้อ3.
+n = 100
+while n >= -100:
+    print(n, end=', ')
+    n -= 10
+
+print()
+
+# ข้อ4.
+n = -30
+while n <= 30:
+    print(n, end=', ')
+    n += 10
+
+print()
+
+# ข้อ5.
+n = 15
+while n <= 55:
+    print(n, end=', ')
+    n += 8
